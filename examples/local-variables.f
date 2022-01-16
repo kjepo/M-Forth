bd2d64a34b740362146dd77c4ceff3d6054ca433
Kjell Post <kjell@irstafoto.se>
2022-01-16 04:41:29 +0100

    Generalised it

--- examples/local-variables.f
+++ examples/local-variables.f
@@ -1,44 +1,47 @@
 ( A few words on arrays )
 : ARRAY ( n -- ) CELLS ALLOT CONSTANT ;
-: INDEX! ( a n -- ) CELLS + ! ;
-: INDEX@ ( a n -- a[n] ) CELLS + @ ;
+: [!] ( a n -- ) CELLS + ! ;
+: [@] ( a n -- a[n] ) CELLS + @ ;
+
+( The number of local variables in a frame is 26: a ... z )
+26 CONSTANT #LOCAL-VARS
 
 ( Create space for 100 frames of local variables a ... z )
-2600 ARRAY LOCAL-VARS
+#LOCAL-VARS 100 * ARRAY LOCAL-VARS
 
-( Use >a to create a local variable a, use a> to push value of a on stack )
-: >a ( n -- ) LOCAL-VARS  0 INDEX! ;  : a> ( n -- ) LOCAL-VARS  0 INDEX@ ;
-: >b ( n -- ) LOCAL-VARS  1 INDEX! ;  : b> ( n -- ) LOCAL-VARS  1 INDEX@ ;
-: >c ( n -- ) LOCAL-VARS  2 INDEX! ;  : c> ( n -- ) LOCAL-VARS  2 INDEX@ ;
-: >d ( n -- ) LOCAL-VARS  3 INDEX! ;  : d> ( n -- ) LOCAL-VARS  3 INDEX@ ;
-: >e ( n -- ) LOCAL-VARS  4 INDEX! ;  : e> ( n -- ) LOCAL-VARS  4 INDEX@ ;
-: >f ( n -- ) LOCAL-VARS  5 INDEX! ;  : f> ( n -- ) LOCAL-VARS  5 INDEX@ ;
-: >g ( n -- ) LOCAL-VARS  6 INDEX! ;  : g> ( n -- ) LOCAL-VARS  6 INDEX@ ;
-: >h ( n -- ) LOCAL-VARS  7 INDEX! ;  : h> ( n -- ) LOCAL-VARS  7 INDEX@ ;
-: >i ( n -- ) LOCAL-VARS  8 INDEX! ;  : i> ( n -- ) LOCAL-VARS  8 INDEX@ ;
-: >j ( n -- ) LOCAL-VARS  9 INDEX! ;  : j> ( n -- ) LOCAL-VARS  9 INDEX@ ;
-: >k ( n -- ) LOCAL-VARS 10 INDEX! ;  : k> ( n -- ) LOCAL-VARS 10 INDEX@ ;
-: >l ( n -- ) LOCAL-VARS 11 INDEX! ;  : l> ( n -- ) LOCAL-VARS 11 INDEX@ ;
-: >m ( n -- ) LOCAL-VARS 12 INDEX! ;  : m> ( n -- ) LOCAL-VARS 12 INDEX@ ;
-: >n ( n -- ) LOCAL-VARS 13 INDEX! ;  : n> ( n -- ) LOCAL-VARS 13 INDEX@ ;
-: >o ( n -- ) LOCAL-VARS 14 INDEX! ;  : o> ( n -- ) LOCAL-VARS 14 INDEX@ ;
-: >p ( n -- ) LOCAL-VARS 15 INDEX! ;  : p> ( n -- ) LOCAL-VARS 15 INDEX@ ;
-: >q ( n -- ) LOCAL-VARS 16 INDEX! ;  : q> ( n -- ) LOCAL-VARS 16 INDEX@ ;
-: >r ( n -- ) LOCAL-VARS 17 INDEX! ;  : r> ( n -- ) LOCAL-VARS 17 INDEX@ ;
-: >s ( n -- ) LOCAL-VARS 18 INDEX! ;  : s> ( n -- ) LOCAL-VARS 18 INDEX@ ;
-: >t ( n -- ) LOCAL-VARS 19 INDEX! ;  : t> ( n -- ) LOCAL-VARS 19 INDEX@ ;
-: >u ( n -- ) LOCAL-VARS 20 INDEX! ;  : u> ( n -- ) LOCAL-VARS 20 INDEX@ ;
-: >v ( n -- ) LOCAL-VARS 21 INDEX! ;  : v> ( n -- ) LOCAL-VARS 21 INDEX@ ;
-: >w ( n -- ) LOCAL-VARS 22 INDEX! ;  : w> ( n -- ) LOCAL-VARS 22 INDEX@ ;
-: >x ( n -- ) LOCAL-VARS 23 INDEX! ;  : x> ( n -- ) LOCAL-VARS 23 INDEX@ ;
-: >y ( n -- ) LOCAL-VARS 24 INDEX! ;  : y> ( n -- ) LOCAL-VARS 24 INDEX@ ;
-: >z ( n -- ) LOCAL-VARS 25 INDEX! ;  : z> ( n -- ) LOCAL-VARS 25 INDEX@ ;
+( Use >x to create a local variable x, use x> to push value of x on stack )
+: >a LOCAL-VARS  0 [!] ;  : a> LOCAL-VARS  0 [@] ;
+: >b LOCAL-VARS  1 [!] ;  : b> LOCAL-VARS  1 [@] ;
+: >c LOCAL-VARS  2 [!] ;  : c> LOCAL-VARS  2 [@] ;
+: >d LOCAL-VARS  3 [!] ;  : d> LOCAL-VARS  3 [@] ;
+: >e LOCAL-VARS  4 [!] ;  : e> LOCAL-VARS  4 [@] ;
+: >f LOCAL-VARS  5 [!] ;  : f> LOCAL-VARS  5 [@] ;
+: >g LOCAL-VARS  6 [!] ;  : g> LOCAL-VARS  6 [@] ;
+: >h LOCAL-VARS  7 [!] ;  : h> LOCAL-VARS  7 [@] ;
+: >i LOCAL-VARS  8 [!] ;  : i> LOCAL-VARS  8 [@] ;
+: >j LOCAL-VARS  9 [!] ;  : j> LOCAL-VARS  9 [@] ;
+: >k LOCAL-VARS 10 [!] ;  : k> LOCAL-VARS 10 [@] ;
+: >l LOCAL-VARS 11 [!] ;  : l> LOCAL-VARS 11 [@] ;
+: >m LOCAL-VARS 12 [!] ;  : m> LOCAL-VARS 12 [@] ;
+: >n LOCAL-VARS 13 [!] ;  : n> LOCAL-VARS 13 [@] ;
+: >o LOCAL-VARS 14 [!] ;  : o> LOCAL-VARS 14 [@] ;
+: >p LOCAL-VARS 15 [!] ;  : p> LOCAL-VARS 15 [@] ;
+: >q LOCAL-VARS 16 [!] ;  : q> LOCAL-VARS 16 [@] ;
+: >r LOCAL-VARS 17 [!] ;  : r> LOCAL-VARS 17 [@] ;
+: >s LOCAL-VARS 18 [!] ;  : s> LOCAL-VARS 18 [@] ;
+: >t LOCAL-VARS 19 [!] ;  : t> LOCAL-VARS 19 [@] ;
+: >u LOCAL-VARS 20 [!] ;  : u> LOCAL-VARS 20 [@] ;
+: >v LOCAL-VARS 21 [!] ;  : v> LOCAL-VARS 21 [@] ;
+: >w LOCAL-VARS 22 [!] ;  : w> LOCAL-VARS 22 [@] ;
+: >x LOCAL-VARS 23 [!] ;  : x> LOCAL-VARS 23 [@] ;
+: >y LOCAL-VARS 24 [!] ;  : y> LOCAL-VARS 24 [@] ;
+: >z LOCAL-VARS 25 [!] ;  : z> LOCAL-VARS 25 [@] ;
 
 ( Start a new frame for local variables a ... z )
-: LSP-> 26 +TO LOCAL-VARS ;
+: LSP-> #LOCAL-VARS +TO LOCAL-VARS ;
 
 ( End most recent frame )
-: <-LSP -26 +TO LOCAL-VARS ;
+: <-LSP #LOCAL-VARS NEGATE +TO LOCAL-VARS ;
 
 ( Use :: and ;; to start and define new words that use local variables a...z )
 : :: IMMEDIATE : ' LSP-> , ;
